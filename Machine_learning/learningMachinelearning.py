4b9cc7630cf18ef0c3fc091caf7133f4d5f565c0
shakiran-nannyombi <devkiran256@gmail.com>
2025-06-04 09:50:25 +0300

    Introduction to Machine Learning

---
+++ Machine_learning/learningMachinelearning.py
@@ -0,0 +1,48 @@
+# application of Machine lerning
+# 1. image  analysis
+# 2. speech recognition
+# 3. natural language processing
+# 4. recommender systems
+# 5. fraud detection
+# 6. autonomous vehicles
+# 7. healthcare diagnostics
+# 8. financial forecasting
+# 9. robotics
+
+#Types of Machine Learning
+# 1. Supervised Learning
+'''
+common algorithms in Supervised Learning include:
+- Linear Regression
+- Logistic Regression
+- Decision Trees
+- Random Forests
+- Support Vector Machines (SVM)
+- Neural Networks
+- deep learning(CNNs, RNNs) - image classification, time series analysis
+- Gradient Boosting Machines (GBM)
+'''
+# 2. Unsupervised Learning
+'''
+common algorithms in Unsupervised Learning include:
+- K-Means Clustering
+- Hierarchical Clustering
+- Principal Component Analysis (PCA)
+- t-Distributed Stochastic Neighbor Embedding (t-SNE)
+'''
+# 3. Semi-supervised Learning
+'''
+common algorithms in Semi-supervised Learning include:
+- Semi-supervised Support Vector Machines (S3VM)
+- Graph-based methods
+- Co-training
+'''
+# 4. Reinforcement Learning
+'''
+common algorithms in Reinforcement Learning include:
+- Q-Learning
+- Deep Q-Networks (DQN)
+- Proximal Policy Optimization (PPO)
+- Actor-Critic methods  
+- Monte Carlo methods
+'''
